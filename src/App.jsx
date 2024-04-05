7e550bbabc8e454a22a3f4202fe13ed01cae8f20
developerdev2007 <developerdev2007@gmail.com>
2024-04-05 13:03:03 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,28 @@
+import {
+
+  Hero,
+    Footer,
+    CustomerReviews,
+    Nav,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe
+} from "./sections";
 const App = () => {
   return (
     <main className="relative">
-    Nav
-    <section className="x1:padding-1 wide:padding-r padding-b">
-      Hero
-    </section>
-    <section className="padding">
-      PopularProducts
-    </section>
-    <section className="padding">
-      SuperQuality
-    </section>
-    <section className="padding">
-  Services
-    </section>
-    <section className="padding">
-  SpecialOffers
-    </section>
-    <section className="padding">
-  SpecialOffers
-    </section>
-    <section className="padding">
-  SpecialOffers
-    </section>
-    <section className="padding">
-  SpecialOffers
-    </section>
-    <section className="padding">
-  SpecialOffers
-    </section>
-    
+      Nav
+      <section className="x1:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding"><PopularProducts></section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding">Services</section>
+      <section className="padding">SpecialOffers</section>
+      <section className="padding">CustomerReviews</section>
+      <section className="padding">Subscribe</section>
+    <section className="padding">Footer</section>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
